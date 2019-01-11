923212f8ffd7d61996ad9e39ec8609708c20eb96
岳文涛 <yuewentao@meizu.com>
2019-01-11 10:31:03 +0800

    111

--- MyTest/app/src/main/java/com/example/yuewentao/mytest/MainActivity.java
+++ MyTest/app/src/main/java/com/example/yuewentao/mytest/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.yuewentao.mytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "sdfjasdfhx新加", Toast.LENGTH_SHORT).show();
+        btn=findViewById(R.id.btn);
     }
 }
